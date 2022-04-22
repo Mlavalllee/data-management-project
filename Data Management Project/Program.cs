a71989cecb4b9786ae6c408ebaf285d041d6bda6
Mlavalllee <83092482+Mlavalllee@users.noreply.github.com>
2022-04-22 14:48:41 -0600

    Add files via upload

---
+++ Data Management Project/Program.cs
@@ -0,0 +1,54 @@
+﻿
+string[,] Songs =
+{
+  // genre || songs
+    {"pop", "test"},
+    {"rap", "test"},
+    {"rock", "test"},
+    {"eurobeat", "test"},
+    {"futurefunk", "test"},
+    {"jazz", "test"},
+    {"country", "test"},
+    {"jpop", "test"},
+    {"trap", "test"},
+    {"edm", "test"},
+    {"reggae", "test"},
+    {"lofi", "test"},
+    {"r&b", "test"},
+    {"classic", "test"}
+};
+
+SignIn();
+
+void SignIn()
+{
+    // User Sign in / regristration 
+    Console.WriteLine();
+    Console.WriteLine("Already have an account? please sign in");
+    Console.WriteLine("1: Sign In");
+    Console.WriteLine("2: Register Account");
+    Console.Write("input: ");
+    string input = Console.ReadLine();
+    int num = int.Parse(input);
+    Console.Clear();
+    switch (num)
+    {
+        case 1:
+            Console.WriteLine("");
+            break;
+        case 2:
+            Console.WriteLine("");
+            break;
+        default:
+            Console.WriteLine("sign in type not chosen, please try again");
+            break;
+    }
+    SignIn();
+}
+
+
+
+void MenuLoop()
+{
+    MenuLoop();
+}
